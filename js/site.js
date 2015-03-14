4b860b5d07cd25a445cd76897c927327d21a2a89
Andrey Viktorov <andreyviktorov@outlook.com>
2015-03-14 20:08:49 +0700

    Updated

--- js/site.js
+++ js/site.js
@@ -1,13 +1,3 @@
-// Center inner block
-$.fn.centerInner = function(inner) {
-  content = this.children(inner);
-  this.css("position", "relative");
-  content.css("position", "absolute");
-  content.css("left", (this.width() - content.width()) / 2);
-  content.css("top", (this.height() - content.height()) / 2);
-  return this;
-}
-
 var scrollTo = function(elem) {
   $('html, body').animate({
     scrollTop: $(elem).offset().top
@@ -16,9 +6,7 @@ var scrollTo = function(elem) {
 
 $(document).ready(function() {
   var resizeCallback = function() {
-    $("#slide-one").centerInner(".inner");
-    $("#slide-two").centerInner(".inner");
-    $("#slide-three").centerInner(".inner");
+    $(".slide").centerChild(".inner");
   };
 
   $(window).on('resize orientationChanged', resizeCallback);
