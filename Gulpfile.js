702b21dd46b0b91f85cd96a832ab39c893100e29
Andrey Viktorov <andreyviktorov@outlook.com>
2016-04-15 18:52:05 +0700

    Some changez

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require("gulp"),
     less = require("gulp-less"),
     path = require("path"),
-    exec = require('child_process').exec;
+    exec = require("child_process").exec,
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify");
 
 var paths = {
   less: ['./less/**/*.less'],
-  riot: ['./js/**/*.tag']
+  riot: ['./js/**/*.tag'],
+  riotcompiled: ['./riot/**/*.js']
 }
 
 gulp.task('less', function() {
@@ -13,16 +16,20 @@ gulp.task('less', function() {
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
 });
 
 // riot-gulp doesn't work with coffee, idk why
 gulp.task('riot', function() {
-	return exec('riot --type coffee --expr "js"');
+  exec('riot --type coffee --expr "riot"');
+  return gulp.src(paths.riotcompiled)
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./js'))
 });
 
 gulp.task('watch', function() {
-	gulp.start('default');
+  gulp.start('default');
   gulp.watch(paths.less, ['less']);
   gulp.watch(paths.riot, ['riot'])
 });
